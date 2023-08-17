9fe4c87df1080fce66a8f2cb73ce1b11bea5a94e
Kozlov Nikita <day161184@gmail.com>
2023-08-17 16:04:04 +0300

    last commit

--- script.js
+++ script.js
@@ -16,35 +16,46 @@ const drawList = (data, listType) => {
         <span>Description:</span>
         <p class='description'>${card.description}</p>
         <br />
-        <button class='button nextButton'>Next</button>
+        ${
+          listType !== 'deleted'
+            ? `<button class='button btnEdit'>Edit</button>
+              <button class='button btnNext'>Next</button>
+              <button class='button btnDelete'>Delete</button>`
+            : `<button class='button btnRestore'>Restore</button>`
+        }
       </li>
     `
   })
 }
 
-const openToDoModal = () => {
+const openToDoModal = (event) => {
+  event.preventDefault()
   modalTodoWrapperEl.style.display = 'flex'
 }
 
-const closeTodoModal = (event) => {
+const closeTodoModal = (event, modalName) => {
   event.preventDefault()
-  modalTodoWrapperEl.style.display = 'none'
+  modalName.style.display = 'none'
 }
 
-btnClose.addEventListener('click', closeTodoModal)
+btnClose.addEventListener('click', (event) => {
+  event.preventDefault()
+  closeTodoModal(event, modalTodoWrapperEl)
+})
 
 // write a function to add a todo item
+
 const handleCreateToDoForm = (event, data) => {
   event.preventDefault()
+
   // Get the values from the form
   const title = inputTitleEl.value
   const description = inputDescriptionEl.value
+
   // Do something with the form data (e.g., send it to the server)
   data.toDo.push({id: Math.random(), title, description})
-  console.log(data.toDo)
   createToDoFormEl.reset()
   modalTodoWrapperEl.style.display = 'none'
-
   drawList(data, 'toDo')
 }
 
@@ -56,6 +67,45 @@ const commonVariables = (eventTarget, data) => {
   const deletedCardIndex = data[listId].findIndex((card) => card.id === deletedCard.id)
 
   return {listId, deletedCard, deletedCardIndex}
+}
+
+const editCard = (eventTarget, data) => {
+  const modal = document.querySelector('.modalEditTodoWrapper')
+  const inputEditTitle = document.querySelector('#inputEditTitle')
+  const inputEditDescription = document.querySelector('#inputEditDescription')
+  const btnEdit = document.querySelector('#btnEdit')
+  const btnClose = document.querySelector('#modalEditTodo #btnClose')
+
+  const {listId, deletedCard, deletedCardIndex} = commonVariables(eventTarget, data)
+
+  modal.style.display = 'flex'
+  inputEditTitle.value = deletedCard.title
+  inputEditDescription.value = deletedCard.description
+
+  const editListenerHandler = (event) => {
+    event.preventDefault()
+    data[listId].splice(deletedCardIndex, 1, {
+      title: inputEditTitle.value,
+      description: inputEditDescription.value,
+      id: deletedCard.id,
+    })
+
+    closeModal()
+  }
+
+  const closeModal = () => {
+    btnEdit.removeEventListener('click', editListenerHandler)
+
+    modal.style.display = 'none'
+    drawList(data, listId)
+  }
+
+  btnEdit.addEventListener('click', editListenerHandler)
+
+  btnClose.addEventListener('click', (event) => {
+    event.preventDefault()
+    closeModal()
+  })
 }
 
 const tranferCardToAnotherList = (eventTarget, data) => {
@@ -73,6 +123,31 @@ const tranferCardToAnotherList = (eventTarget, data) => {
   drawList(data, listsId[nextListIndex])
 }
 
+const deleteCard = (eventTarget, data) => {
+  const {listId, deletedCard, deletedCardIndex} = commonVariables(eventTarget, data)
+
+  data[listId].splice(deletedCardIndex, 1)
+  data.deleted.push(deletedCard)
+
+  drawList(data, listId)
+  drawList(data, 'deleted')
+}
+
+const deleteAllFromDeletedList = (data) => {
+  data.deleted = []
+  drawList(data, 'deleted')
+}
+
+const restoreCard = (eventTarget, data) => {
+  const {deletedCard, deletedCardIndex} = commonVariables(eventTarget, data)
+
+  data.deleted.splice(deletedCardIndex, 1)
+  data.toDo.push(deletedCard)
+
+  drawList(data, 'deleted')
+  drawList(data, 'toDo')
+}
+
 const init = () => {
   const todoContainerEl = document.querySelector('.toDoContainer')
   const btnOpenModelEl = document.querySelector('#btnOpenModal')
@@ -87,9 +162,23 @@ const init = () => {
   btnOpenModelEl.addEventListener('click', openToDoModal)
   createToDoFormEl.addEventListener('submit', (event) => handleCreateToDoForm(event, todos))
   todoContainerEl.addEventListener('click', (event) => {
+    const eventTarget = event.target
+
     switch (true) {
-      case [...event.target.classList].includes('nextButton'):
-        tranferCardToAnotherList(event.target, todos)
+      case [...eventTarget.classList].includes('btnDelete'):
+        deleteCard(eventTarget, todos)
+        break
+      case [...eventTarget.classList].includes('btnEdit'):
+        editCard(eventTarget, todos)
+        break
+      case [...eventTarget.classList].includes('btnNext'):
+        tranferCardToAnotherList(eventTarget, todos)
+        break
+      case [...eventTarget.classList].includes('btnRestore'):
+        restoreCard(eventTarget, todos)
+        break
+      case [...eventTarget.classList].includes('btnClearAll'):
+        deleteAllFromDeletedList(todos)
         break
     }
   })
